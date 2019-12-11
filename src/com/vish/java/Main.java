3a5969a8d22053dd660cce4d77200a7102395d64
Vishroy Seenarain <vishseenarain@gmail.com>
2019-12-12 02:37:25 +0400

    Added :
    * Cleaner code
    * addMethod
    * getNthInput

--- src/com/vish/java/Main.java
+++ src/com/vish/java/Main.java
@@ -14,56 +14,73 @@ public class Main {
     private static Scanner scanner;
     private static int numOfInputs;
     private static String[] arrayWords;
-    private static String allWords;
+    private static int index;
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
 
+        System.out.println(" ");
+
+        System.out.println("-----------------------------------------------------");
+        System.out.println(" Program to get inputs depending on how many you need ");
+        System.out.println("-----------------------------------------------------");
+        System.out.println("");
+
         System.out.print("Enter number of inputs you want : ");
         numOfInputs = scanner.nextInt();
 
-        System.out.println("Displaying all names:");
-        for (String names : getInputs()){
-            System.out.println(names);
-        }
+        System.out.println(" ");
 
-        System.out.println("Displaying names from back:");
-        System.out.println(getWordsFromLast());
+        System.out.println("Adding words... ");
+        addInputs(numOfInputs);
+
+        System.out.println(" ");
+
+
+        System.out.print("Displaying added items : ");
+        for (String names : getInputs()){
+            System.out.print(names + " ");
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+
+        System.out.print("Enter number to get specific element :");
+        index = scanner.nextInt();
+
+        System.out.print("Item at " + index + " is : " + getNthInput(index));
 
     }
 
 
 
-    public static String[] getInputs(){
-        arrayWords = new String[numOfInputs];
-        String word;
 
-        for(int i = 0; i < numOfInputs; i++){
-            System.out.print("Enter name to add : ");
+    /* Manipulate the input */
+    public static void addInputs(int numOfInputs){
+        String word;
+        arrayWords = new String[numOfInputs];
+
+        for (int i = 0; i < numOfInputs; i++){
+            System.out.print("Enter name : ");
             word = scanner.next();
 
             arrayWords[i] = word;
-
-            System.out.println("Added : " + arrayWords[i]);
-            System.out.println(" ");
         }
-
-        return arrayWords;
     }
 
-
-
-    public static String[] getWordsFromLast(){
+    public static String[] getInputs(){
         if (arrayWords.length != 0){
-            String[] singleString = new String[numOfInputs];
-
-            for (int i = arrayWords.length - 1; i >= 0; i--){
-                singleString[0] = arrayWords[i];
-                return singleString;
-            }
+            return arrayWords;
         }
 
         return null;
+    }
+
+    public static String getNthInput(int index){
+        if (arrayWords.length >= index){
+            return arrayWords[index];
+        }
+
+        return "No such index..";
     }
 
 }
